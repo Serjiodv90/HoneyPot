c5c54778a2a639a7409fed334bc4065acf365a6f
serjio90 <serii.d14@gmail.com>
2019-04-11 14:30:24 +0300

    add dependencies for working with mongoDB

---
+++ TrapManagement/src/main/java/trapManagementServer/ftp/dbLink/FtpUsersService.java
@@ -0,0 +1,33 @@
+package trapManagementServer.ftp.dbLink;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import trapManagementServer.ftp.dal.FtpUserDao;
+
+@Service
+public class FtpUsersService {
+
+	private FtpUserDao ftpUserDao;
+	
+	@Autowired
+	public FtpUsersService(FtpUserDao ftpUserDao) {
+		this.ftpUserDao = ftpUserDao;
+	}
+	
+	public List<FtpUser> getAllAllowedFtpUsers() {
+		List<FtpUser> users = new ArrayList<>();
+		this.ftpUserDao.findAll().forEach(user->users.add(user));
+		return users;
+	}
+	
+	public Optional<FtpUser> getUserByName(String name) {
+		return this.ftpUserDao.findById(name);
+	}
+	
+	
+	
+}
