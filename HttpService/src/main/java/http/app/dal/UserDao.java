c04edcd6593a793d0de93c836ceb4e502be6b930
Tair Hassid <tairhassid@gmail.com>
2019-06-08 14:35:15 +0300

    fixed http pom file

--- HttpService/src/main/java/http/app/dal/UserDao.java
+++ HttpService/src/main/java/http/app/dal/UserDao.java
@@ -1,12 +1,11 @@
 package http.app.dal;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
+
 
 import http.app.model.User;
 
-@RepositoryRestResource
+
 public interface UserDao extends CrudRepository<User, String>{
 	
 }
